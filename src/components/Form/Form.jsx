e920c58ad7056e3c9f2c8bc0a9f2c02b8ba372c2
ezequiel stom <ezequielstom@gmail.com>
2025-01-08 13:12:50 -0300

    Agregar animaciones de Tailwind, actualizar imágenes y ajustar estilos en varios componentes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -80,8 +80,8 @@ export function Form() {
 								y1="1"
 								x2="1"
 								y2="0">
-								<stop offset="0" stop-color="#d2e0d7"></stop>
-								<stop offset="1" stop-color="#d2e0d7"></stop>
+								<stop offset="0" stopColor="#d2e0d7"></stop>
+								<stop offset="1" stopColor="#d2e0d7"></stop>
 							</linearGradient>
 						</defs>
 						<path
--- src/components/SwiperHome/SwiperHome.jsx
+++ src/components/SwiperHome/SwiperHome.jsx
@@ -8,18 +8,20 @@ import "./SwiperHome.css";
 
 import arrowLeft from "../../icons/arrowLeft.svg";
 import arrowRight from "../../icons/arrowRight.svg";
-import home1 from "../../img/farmApp.jpg";
+import home1 from "../../img/malvinas.jpg";
 import home2 from "../../img/orange.avif";
 import home3 from "../../img/cow2.jpg";
 import home4 from "../../img/farm3.webp";
 import home5 from "../../img/irrigator.jpg";
 import home6 from "../../img/farm2.jpg";
 
-import banner1 from "../../img/platform1_test.png";
-import banner2 from "../../img/platform2_test.png";
-import banner3 from "../../img/platform3_test.png";
-import banner4 from "../../img/platform4_test.png";
-import banner5 from "../../img/platform5_test.png";
+import banner1 from "../../img/escudo2.png";
+import banner2 from "../../img/escudo2.png";
+import banner3 from "../../img/escudo2.png";
+import banner4 from "../../img/escudo2.png";
+import banner5 from "../../img/escudo2.png";
+
+import logo from "../../img/nicov-title.png";
 
 export function SwiperHome() {
 	const swiperRef = useRef(null); // Crear referencia para el Swiper
@@ -136,12 +138,13 @@ export function SwiperHome() {
 								</aside>
 								<div className=" home-data absolute flex flex-col items-center md:items-start text-start z-10 gap-5 text-balance px-4 md:px-0 max-w-[40ch] lg:max-w-[60ch] md:top-1/2 md:transform md:-translate-y-1/2">
 									<h1
-										className="home-title bg-gradient-to-r from-white via-[#c684ff] to-[#8e33ff] px-1 text-transparent bg-clip-text text-[3.75rem] md:text-[4vw] max-w-[15ch] leading-none tracking-tight font-black text-center md:text-left -my-4 drop-shadow-md py-4 "
+										className="home-title bg-gradient-to-r from-white via-blue-500 to-blue-800 px-1 text-transparent bg-clip-text text-[3.75rem] md:text-[4vw] max-w-[15ch] leading-none tracking-tight font-black text-center md:text-left -my-4 drop-shadow-md py-4 "
 										data-swiper-parallax="-400">
-										{image.title}
+										{/* {image.title} */}
+										<img src={logo.src} alt="logo" />
 									</h1>
 									<h3
-										className="home-subtitle bg-gradient-to-r from-white  to-[#8e33ff] p-1 text-transparent bg-clip-text font-black text-3xl md:text-4xl  sm:leading-10 text-center sm:text-start tracking-tight drop-shadow-md "
+										className="home-subtitle bg-gradient-to-r from-white  to-blue-800 p-1 text-transparent bg-clip-text font-black text-3xl md:text-4xl  sm:leading-10 text-center sm:text-start tracking-tight drop-shadow-md "
 										data-swiper-parallax="-300">
 										{image.subtitle}
 									</h3>
@@ -159,12 +162,12 @@ export function SwiperHome() {
 									className="parallax-bg absolute top-0 left-0 w-full h-full object-cover object-center -z-10 brightness-[.85] blur-sm"
 									data-swiper-parallax="0%"
 								/>
-								<div className="book-container ">
+								<div className="book-container w-full relative">
 									<img
 										id="img2"
 										src={image.img2.src}
 										alt={`${image.img2} image`}
-										className="absolute md:block hidden  m-auto -right-[25rem]  object-cover sm:rounded-[3rem] -z-10 opacity-85  p-4 shadow-md book "
+										className="absolute md:block hidden  m-auto !-right-[25rem] object-right -z-10 p-4 w-[25vw] book"
 									/>
 								</div>
 							</article>
